532fdaf546c9e69f5abeaa71b8b25838edf59dfe
mudimurtala <ibnmuhyideen95@gmail.com>
2024-06-24 17:39:22 +0100

    Add resetGame() function to reset the scores to zero again

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
 
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+}
+
 function getComputerChoice() {
     let plays = ["rock", "paper", "scissors"];
     const result = Math.floor(Math.random() * 3) + 1;
@@ -15,31 +20,31 @@ function getComputerChoice() {
 
 
 function playRound(humanChoice, computerChoice) {
-        const resultBox = document.getElementsByClassName('result');
-        let resultText = `Human: ${humanChoice}, Computer: ${computerChoice}. `;
+    const resultBox = document.getElementsByClassName('result');
+    let resultText = `Human: ${humanChoice}, Computer: ${computerChoice}. `;
         
-        if (humanChoice === computerChoice) {
-            resultText += "No winner for this round.";
-        } else if ((humanChoice === "rock" && computerChoice === "paper") ||
-                   (humanChoice === "paper" && computerChoice === "scissors") ||
-                   (humanChoice === "scissors" && computerChoice === "rock")) {
-            computerScore++;
-            resultText += "Computer wins this round.";
-        } else {
-            humanScore++;
-            resultText += "You win this round.";
-        }
-    
-        resultText += `<br>Scores -> Human: ${humanScore}, Computer: ${computerScore}`;
-    
-        if (humanScore === 5) {
-            resultText += "<br><strong>You win the game, Congratulations!</strong>";
-            resetGame();
-        } else if (computerScore === 5) {
-            resultText += "<br><strong>Computer wins the game!</strong>";
-            resetGame();
-        }
-        
-        resultBox.innerHTML = resultText;
+    if (humanChoice === computerChoice) {
+        resultText += "No winner for this round.";
+    } else if ((humanChoice === "rock" && computerChoice === "paper") ||
+                (humanChoice === "paper" && computerChoice === "scissors") ||
+                (humanChoice === "scissors" && computerChoice === "rock")) {
+        computerScore++;
+        resultText += "Computer wins this round.";
+    } else {
+        humanScore++;
+        resultText += "You win this round.";
     }
+    
+    resultText += `<br>Scores -> Human: ${humanScore}, Computer: ${computerScore}`;
+    
+    if (humanScore === 5) {
+        resultText += "<br><strong>You win the game, Congratulations!</strong>";
+        resetGame();
+    } else if (computerScore === 5) {
+        resultText += "<br><strong>Computer wins the game!</strong>";
+    resetGame();
+    }
+        
+    resultBox.innerHTML = resultText;
+}
 
